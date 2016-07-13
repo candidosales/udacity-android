faea42a8cbfeb9e38aaa4733cbcd53a76472fad3
candidosales <candidosg@gmail.com>
2016-07-13 09:43:30 -0300

    buttons

--- app/src/main/java/com/example/candidosg/portfolio/MainActivity.java
+++ app/src/main/java/com/example/candidosg/portfolio/MainActivity.java
@@ -5,9 +5,18 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Context context = getApplicationContext();
+    CharSequence text = "Hello toast!";
+    int duration = Toast.LENGTH_SHORT;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
